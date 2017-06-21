8d9be45e5e1c85f8315e6859a73c7679a97b10ef
casper chang <casper_chang@wistron.com>
2017-06-21 13:26:06 +0800

    add unit test function

--- lunch/stores/tests.py
+++ lunch/stores/tests.py
@@ -1,3 +1,9 @@
 from django.test import TestCase
 
 # Create your tests here.
+class HomeViewTests(TestCase):
+    
+    def test_home_view(self):
+        response = self.client.get('/')
+        self.assertEqual(response.status_code, 200)
+        self.assertTemplateUsed(response, 'home.html')
