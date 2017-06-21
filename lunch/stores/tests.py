776ac4e229d73538a62b96c642c0c0086b916b41
casper chang <casper_chang@wistron.com>
2017-06-21 17:23:53 +0800

    add MORE unit test function

--- lunch/stores/tests.py
+++ lunch/stores/tests.py
@@ -1,5 +1,5 @@
 from django.test import TestCase
-
+from .models import Store, MenuItem
 
 '''
 # Move to pages/views.py
@@ -11,3 +11,26 @@ class HomeViewTests(TestCase):
         self.assertEqual(response.status_code, 200)
         self.assertTemplateUsed(response, 'home.html')
 '''
+
+class StoreViewTests(TestCase):
+    def setUp(self): 
+        print('!!! test setup')
+        Store.objects.create(name='肯德基', notes='沒有薄皮嫩雞倒一倒算了啦')
+        mcdonalds = Store.objects.create(name='McDonalds') 
+        MenuItem.objects.create(store=mcdonalds, name='大麥克餐', price=99)
+        
+    def tearDown(self): 
+        print('!!! test tear down')
+        Store.objects.all().delete()
+
+    def test_list_view(self): 
+        print('!!! test_list_view')
+
+        r = self.client.get('/store/') 
+        self.assertContains(r, '<a class="navbar-brand" href="/">午餐系統</a>', html=True, ) 
+        self.assertContains(r, '<a href="/store/1/">肯德基</a>', html=True) 
+        self.assertContains(r, '沒有薄皮嫩雞倒一倒算了啦')
+
+        response = self.client.get('/store/2/') 
+        self.assertContains( response, '<tr><td>大麥克餐</td><td>99</td></tr>', html=True, )
+
